fff1d58d8cd33f7dd46f02bc1978c6a07e60fdb9
Hafiz931 <163367623+Hafiz931@users.noreply.github.com>
2025-05-26 20:26:29 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+// Init project
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // Some legacy browsers choke on 204
+
+// Static files
+app.use(express.static('public'));
+
+// Routing
+app.get("/", function (req, res) {
+
+  res.sendFile(`${__dirname}/views/index.html`);
+
+});
+
+app.get("/api/whoami", function (req, res) {
+  
+  const responseObj = {
+    "ipaddress": req.ip,
+    "language": req.headers["accept-language"],
+    "software": req.headers["user-agent"]
+  };
+
+  res.json(responseObj);
+
+});
+
+// No matching route
+app.use(function(req, res) {
+
+  res.status(404).sendFile(`${__dirname}/views/404.html`);
+
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
